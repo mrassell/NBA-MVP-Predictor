42b41079916ca784e07b8c30c7ddd99f5ab5956a
mrassell <mr6761@nyu.edu>
2025-01-01 23:15:46 -0500

    Create predictor.py
    imports and plan

---
+++ predictor.py
@@ -0,0 +1,9 @@
+#Just going to try 2023 season for now 
+#Collect data about the season 
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+import requests
+from bs4 import BeautifulSoup
+from io import StringIO
